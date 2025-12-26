a17216d82b4240ee572dea134cc6ee07f5fbd979
DimAndAnd <123008948+DimAndAnd@users.noreply.github.com>
2025-12-26 20:19:44 +0200

    create experience page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ShowmoreComponent } from './showmore/showmore.component';
+import { PricesComponent } from './prices/prices.component';
 
 export const routes: Routes = [
+    { path: '', pathMatch: 'full', redirectTo: '/home' },
     {path: 'home', component: HomeComponent},
-    
+    {path: 'show-more', component: ShowmoreComponent},
+    {path: 'prices', component: PricesComponent},    
 ];
---
+++ src/app/showmore/showmore.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-showmore',
+  imports: [],
+  templateUrl: './showmore.component.html',
+  styleUrl: './showmore.component.css'
+})
+export class ShowmoreComponent {
+
+}
