32ed28a39d6a9beda6f3652b50c5666826cc4531
DimAndAnd <123008948+DimAndAnd@users.noreply.github.com>
2026-03-15 18:36:08 +0200

    add scroll to top button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { NavComponent } from './nav/nav.component';
 import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
 import { CertificatesComponent } from './certificates/certificates.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterModule, RouterOutlet, NavComponent, HomeComponent, CertificatesComponent, FooterComponent],
+  imports: [CommonModule, RouterModule, RouterOutlet, NavComponent, HomeComponent, CertificatesComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'andreeva-app';
+  isMenuScrolled = false;
+
+  @HostListener('window:scroll', ['$event'])
+  scrollCheck() {
+    console.log(window.pageYOffset);
+    if (window.pageYOffset > 180) {
+      this.isMenuScrolled = true;
+    }
+    else
+      this.isMenuScrolled = false;
+
+  }
+
+  scrollToTop() {
+    document.body.scrollIntoView({ behavior: 'smooth' })
+  }
+
 }
