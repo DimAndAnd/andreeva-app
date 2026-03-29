592ede71c166b6a455bd1b446ea83b22addd7f87
DimAndAnd <123008948+DimAndAnd@users.noreply.github.com>
2026-03-29 15:22:41 +0300

    update home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -15,4 +15,29 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
      AOS.init();
   }
+
+  delocount: number = 0;
+  clientcount: number = 0;
+  wincount: number = 0;
+
+  delocountstop: any = setInterval(() => {
+    this.delocount++;
+    if (this.delocount == 185) {
+      clearInterval(this.delocountstop)
+    }
+  }, 10)
+
+  clientcountstop: any = setInterval(() => {
+    this.clientcount++;
+    if (this.clientcount == 32) {
+      clearInterval(this.clientcountstop)
+    }
+  }, 50)
+
+  wincountstop: any = setInterval(() => {
+    this.wincount++;
+    if (this.wincount == 128) {
+      clearInterval(this.wincountstop)
+    }
+  }, 20)
 }
